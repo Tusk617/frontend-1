8764699a7f801c9bb666732e0429501ea85c9634
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-08-26 17:59:53 -0500

    commiting before merge

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,17 +1,18 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import { date } from 'yup'
 
 const initialFormValues={
     name: '',
     description:'',
-    duedate: '',
+    date: '',
     frequency:'',
 }
 export const CreateTodo = () => {
     const [form, setForm] = useState(initialFormValues)
     const [toDos, setToDos] = useState([])
     const config = {
-        headers: { Authorization: `Bearer b37f00fc-8e1f-4028-9acf-9a1f74fd7bf9` }
+        headers: { Authorization: `Bearer 6be8995d-c2e5-49ea-8493-d7d0b83f056d` }
     };
     const handleChange = (e) =>{
         setForm({
@@ -24,8 +25,8 @@ export const CreateTodo = () => {
         const newTodo ={
             name: form.name.trim(),
             description:form.description.trim(),
-            duedate: form.duedate.trim(),
-            frequency:form.frequency.trim(),
+            date: form.date.trim(),
+            frequency:form.frequency.trim()
         }
         postNewTodo(newTodo)
         setForm(initialFormValues)
@@ -69,11 +70,12 @@ export const CreateTodo = () => {
                     onChange={handleChange} >
                     </input> */}
                     <select
-                    name='duedate'
-                    value={form.duedate}
-                    onChange={handleChange} >
+                    name='date'
+                    value={form.date}
+                    onChange={handleChange}
+                    >
                         <option value = ''>Select an option</option>
-                        <option value="1969-12-31 16:00:01">An option</option>
+                        <option value='2020-12-31'>An option</option>
                     </select>
                 </label>
                 <label>
